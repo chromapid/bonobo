45780926309740db56de37f42c17d0a9060e8ca0
chromapid <67613147+chromapid@users.noreply.github.com>
2022-06-23 16:26:52 -0600

    Undescriptive commit message

--- inc/bonobo/Core/syntax.h
+++ inc/bonobo/Core/syntax.h
@@ -7,22 +7,45 @@
 
 #include <cassert>
 #include <cstdint>
+#include <limits>
 
 namespace bono {
 
-class SyntaxTerm {
+class SyntaxTermInfo {
+
 	uint32_t data;
 
 public:
-	enum class Type;
-	inline SyntaxTerm::Type SyntaxTerm::getType();
+	enum class Type : uint32_t;
+
+	inline SyntaxTermInfo(Type type, uint32_t data) inline Type getType();
+	inline uint32_t getOtherData();
+
+	const uint32_t TYPE_BITS = 3;
+	const uint32_t TOTAL_BITS = std::numeric_limits<decltype(data)>::digits;
+
+	static_assert(TYPE_BITS <= TOTAL_BITS,
+	              "Type bits cannot exceed total data bits.");
+	static_assert(1 << TYPE_BITS > Type::last,
+	              "Type bits must be sufficient to represent all types.");
+	const uint32_t OTHER_BITS = TOTAL_BITS - TYPE_BITS;
 };
 
-enum class SyntaxTerm::Type : uint32_t {};
+enum class SyntaxTermInfo::Type : uint32_t {
+	functionTerm,
+	variableTerm,
+	constantTerm,
 
-inline SyntaxTerm::Type SyntaxTerm::getType() {
-	assert(data >> (32 - 8) && "");
-	return static_cast<Type>(data >> (32 - 8));
+	last = constantTerm
+};
+
+inline SyntaxTermInfo::Type SyntaxTermInfo::getType() {
+	return static_cast<Type>(data >> OTHER_BITS);
+}
+
+inline uint32_t SyntaxTermInfo::Type SyntaxTermInfo::getOtherData() {
+	const uint32_t MASK = 1 << OTHER_BITS - 1;
+	return data & MASK;
 }
 
 } // namespace bono
